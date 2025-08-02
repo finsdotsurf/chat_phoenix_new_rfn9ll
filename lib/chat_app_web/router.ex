e4270772699d4c6eeadb348adad23f9699af9eda
Fly Dev <dev@fly.io>
2025-08-02 11:53:46 +0000

    fix router to add authenticated chat route

--- lib/chat_app_web/router.ex
+++ lib/chat_app_web/router.ex
@@ -50,6 +50,7 @@ defmodule ChatAppWeb.Router do
 
     live_session :require_authenticated_user,
       on_mount: [{ChatAppWeb.UserAuth, :require_authenticated}] do
+      live "/", ChatLive
       live "/users/settings", UserLive.Settings, :edit
       live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
     end
