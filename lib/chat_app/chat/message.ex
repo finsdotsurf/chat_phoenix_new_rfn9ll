f5e754c944f6b8c1370c601fd28d9d36b679897f
Fly Dev <dev@fly.io>
2025-08-03 15:42:37 +0000

    add room relationship to Message schema

--- lib/chat_app/chat/message.ex
+++ lib/chat_app/chat/message.ex
@@ -7,13 +7,15 @@ defmodule ChatApp.Chat.Message do
     field :user_id, :integer
     field :username, :string
 
+    belongs_to :room, ChatApp.Chat.Room
+
     timestamps()
   end
 
   @doc false
   def changeset(message, attrs) do
     message
-    |> cast(attrs, [:content, :user_id, :username])
+    |> cast(attrs, [:content, :user_id, :username, :room_id])
     |> validate_required([:content, :user_id, :username])
     |> validate_length(:content, min: 1, max: 1000)
     |> validate_length(:username, min: 1, max: 50)
