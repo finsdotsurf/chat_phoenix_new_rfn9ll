ba6de566fb8b979363871eb0f12f04c38ede7b95
Fly Dev <dev@fly.io>
2025-08-03 15:43:23 +0000

    add room management to ChatLive

--- lib/chat_app_web/live/chat_live.ex
+++ lib/chat_app_web/live/chat_live.ex
@@ -3,6 +3,7 @@ defmodule ChatAppWeb.ChatLive do
 
   alias ChatApp.Chat
   alias ChatApp.Chat.Message
+  alias ChatApp.Chat.Room
 
   @impl true
   def mount(_params, _session, socket) do
@@ -11,11 +12,16 @@ defmodule ChatAppWeb.ChatLive do
     end
 
     messages = Chat.list_messages()
+    rooms = Chat.list_rooms()
 
     {:ok,
      socket
      |> assign(:messages_empty?, messages == [])
+     |> assign(:rooms, rooms)
+     |> assign(:current_room, nil)
+     |> assign(:show_room_form, false)
      |> assign(:form, to_form(Chat.change_message(%Message{})))
+     |> assign(:room_form, to_form(Room.changeset(%Room{}, %{})))
      |> stream(:messages, messages)}
   end
 
@@ -30,26 +36,109 @@ defmodule ChatAppWeb.ChatLive do
   end
 
   @impl true
+  def handle_event("validate_room", %{"room" => room_params}, socket) do
+    changeset =
+      %Room{}
+      |> Room.changeset(room_params)
+      |> Map.put(:action, :validate)
+
+    {:noreply, assign(socket, room_form: to_form(changeset))}
+  end
+
+  @impl true
   def handle_event("send_message", %{"message" => message_params}, socket) do
     current_user = socket.assigns.current_scope.user
+    current_room = socket.assigns.current_room
 
-    message_params =
-      Map.merge(message_params, %{
-        "user_id" => current_user.id,
-        "username" => String.split(current_user.email, "@") |> List.first() |> String.capitalize()
-      })
+    if current_room do
+      message_params =
+        Map.merge(message_params, %{
+          "user_id" => current_user.id,
+          "username" =>
+            String.split(current_user.email, "@") |> List.first() |> String.capitalize(),
+          "room_id" => current_room.id
+        })
 
-    case Chat.create_message(message_params) do
-      {:ok, message} ->
+      case Chat.create_message(message_params) do
+        {:ok, message} ->
+          Phoenix.PubSub.broadcast(
+            ChatApp.PubSub,
+            "chat:room:#{current_room.id}",
+            {:new_message, message}
+          )
+
+          {:noreply,
+           socket
+           |> assign(:messages_empty?, false)
+           |> assign(:form, to_form(Chat.change_message(%Message{})))
+           |> stream_insert(:messages, message)}
+
+        {:error, %Ecto.Changeset{} = changeset} ->
+          {:noreply, assign(socket, form: to_form(changeset))}
+      end
+    else
+      {:noreply, put_flash(socket, :error, "Please select a room first")}
+    end
+  end
+
+  @impl true
+  def handle_event("create_room", %{"room" => room_params}, socket) do
+    current_user = socket.assigns.current_scope.user
+
+    room_params = Map.put(room_params, "user_id", current_user.id)
+
+    # Auto-generate room name from URL if not provided
+    room_params =
+      if Map.get(room_params, "name", "") == "" do
+        name = extract_domain_name(room_params["url"])
+        Map.put(room_params, "name", name)
+      else
+        room_params
+      end
+
+    case Chat.create_room(room_params) do
+      {:ok, room} ->
         {:noreply,
          socket
-         |> assign(:messages_empty?, false)
-         |> assign(:form, to_form(Chat.change_message(%Message{})))
-         |> stream_insert(:messages, message)}
+         |> assign(:rooms, [room | socket.assigns.rooms])
+         |> assign(:show_room_form, false)
+         |> assign(:room_form, to_form(Room.changeset(%Room{}, %{})))
+         |> put_flash(:info, "Room created successfully!")}
 
       {:error, %Ecto.Changeset{} = changeset} ->
-        {:noreply, assign(socket, form: to_form(changeset))}
+        {:noreply, assign(socket, room_form: to_form(changeset))}
     end
+  end
+
+  @impl true
+  def handle_event("select_room", %{"id" => room_id}, socket) do
+    room = Enum.find(socket.assigns.rooms, &(&1.id == String.to_integer(room_id)))
+
+    if room do
+      # Unsubscribe from old room if any
+      if socket.assigns.current_room do
+        Phoenix.PubSub.unsubscribe(ChatApp.PubSub, "chat:room:#{socket.assigns.current_room.id}")
+      end
+
+      # Subscribe to new room
+      Phoenix.PubSub.subscribe(ChatApp.PubSub, "chat:room:#{room.id}")
+
+      # Load messages for this room
+      messages = Chat.list_messages_for_room(room.id)
+
+      {:noreply,
+       socket
+       |> assign(:current_room, room)
+       |> assign(:messages_empty?, messages == [])
+       |> stream(:messages, messages, reset: true)}
+    else
+      {:noreply, socket}
+    end
+  end
+
+  @impl true
+  def handle_event("toggle_room_form", _params, socket) do
+    {:noreply, assign(socket, show_room_form: !socket.assigns.show_room_form)}
   end
 
   @impl true
@@ -58,6 +147,20 @@ defmodule ChatAppWeb.ChatLive do
      socket
      |> assign(:messages_empty?, false)
      |> stream_insert(:messages, message)}
+  end
+
+  defp extract_domain_name(url) do
+    case URI.parse(url) do
+      %URI{host: host} when not is_nil(host) ->
+        host
+        |> String.replace("www.", "")
+        |> String.split(".")
+        |> List.first()
+        |> String.capitalize()
+
+      _ ->
+        "Unknown Site"
+    end
   end
 
   defp format_timestamp(datetime) do
